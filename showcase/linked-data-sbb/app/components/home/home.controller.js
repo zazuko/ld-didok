c6b45ccd30144c6e71d475e8f8cdb7632371aa47
ue61674 <marius.schaer@sbb.ch>
2016-05-04 07:30:35 +0200

    added some helpful links

--- showcase/linked-data-sbb/app/components/home/home.controller.js
+++ showcase/linked-data-sbb/app/components/home/home.controller.js
@@ -7,6 +7,14 @@ angular.module('home', ['ngResource'])
     .controller('HomeController', function ($scope) {
 
         $scope.world = "Home";
+        $scope.wikiLinkRdf = "https://en.wikipedia.org/wiki/Resource_Description_Framework";
+        $scope.wikiTextRdf = "Resource Description Framework (RDF)";
+
+        $scope.wikiLinkLD = "https://en.wikipedia.org/wiki/Linked_Data";
+        $scope.wikiTextLD = "Linked Data";
+
+        $scope.dataSbbLink = "https://data.sbb.ch";
+        $scope.dataSbbText = "data.sbb.ch"
 
         $scope.function = function (index) {
             //Do something
