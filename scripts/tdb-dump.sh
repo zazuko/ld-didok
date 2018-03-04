5acd66131e7ac19d766a88b48c3342440ac549b7
Adrian Gschwend <ktk@netlabs.org>
2018-03-04 16:10:54 +0100

    Update builds

--- scripts/tdb-dump.sh
+++ scripts/tdb-dump.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tdbdump --loc target/tdb_didok | sed '\#example.org#d' | rapper -i nquads -o ntriples  - http://example.org/base/ > target/everything.nt
+tdbdump --loc target/tdb | sed '\#example.org#d' | rapper -i nquads -o ntriples  - http://example.org/base/ > target/everything.nt
