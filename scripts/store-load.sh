fe8695d739d94cc4672e8266df1dce23bbaff113
Adrian Gschwend <ktk@netlabs.org>
2018-07-23 15:27:25 +0200

    Trying to understand Gitlab API

--- scripts/store-load.sh
+++ scripts/store-load.sh
@@ -1,3 +1,4 @@
 echo https://gitlab.zazuko.com//opentransportdata/ld-didok/-/jobs/$CI_JOB_ID/artifacts/download?private_token=$CI_JOB_TOKEN
+echo https://gitlab.zazuko.com/projects/$CI_PROJECT_ID/jobs/artifacts/$CI_JOB_ID/download?job=name
 #curl -n -X POST -u zazuko:$STORE_PASSWORD --data timeout=1000000  --data-urlencode update="CLEAR GRAPH <http://data.alod.ch/graph/bar> " https://test.lindas-data.ch:8443/alod/update
 #curl -n -X POST -u zazuko:$STORE_PASSWORD --data timeout=1000000  --data-urlencode update="LOAD <https://sos-ch-dk-2.exo.io/zazuko-ingress/alod/everything.nt.gz> INTO GRAPH <http://data.alod.ch/graph/bar>" https://test.lindas-data.ch:8443/alod/update
