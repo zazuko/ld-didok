ee5e944f1a7b375fab5d07909c313e572cb99de9
Adrian Gschwend <ktk@netlabs.org>
2018-10-22 15:41:32 +0200

    Replacing old s3-script

--- scripts/store-s3.sh
+++ scripts/store-s3.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
-s3cmd -c config/s3cmd put target/everything.nt.gz s3://zazuko-staging/didok.nt.gz
-s3cmd -c config/s3cmd setacl --acl-public s3://zazuko-staging/didok.nt.gz
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Using endpoint: $ENDPOINT"
+
+s3cmd -c config/s3cmd put target/everything.nt.gz "s3://zazuko-staging/ssz-$CI_COMMIT_REF_SLUG.nt.gz"
+s3cmd -c config/s3cmd setacl --acl-public "s3://zazuko-staging/ssz-$CI_COMMIT_REF_SLUG.nt.gz"
 
 # Stardog
+curl -u $STORE_USER:$STORE_PASSWORD -X POST -w "%{http_code}" -s -o /dev/null --data timeout=1000000  --data-urlencode update="CLEAR GRAPH <https://linked.opendata.swiss/graph/FOT/didok>>" "$ENDPOINT/update"
+curl -u $STORE_USER:$STORE_PASSWORD -X POST -w "%{http_code}" -s -o /dev/null --data timeout=1000000  --data-urlencode update="LOAD <https://sos-ch-dk-2.exo.io/zazuko-staging/ssz-$CI_COMMIT_REF_SLUG.nt.gz> INTO GRAPH <https://linked.opendata.swiss/graph/FOT/didok>>" "$ENDPOINT/update"
+COUNT=$(curl -u $STORE_USER:$STORE_PASSWORD -H "Accept: application/sparql-results+json" -X POST -s --data timeout=1000000  --data-urlencode query="SELECT (COUNT(?s) AS ?count) WHERE {  GRAPH <https://linked.opendata.swiss/graph/FOT/didok>> {?s ?p ?o}}" "$ENDPOINT/query" | jq '.results.bindings[0].count.value')
 
-curl -u $STORE_USER:$STORE_PASSWORD -X POST -w "%{http_code}" -s -o /dev/null --data timeout=1000000  --data-urlencode update="CLEAR GRAPH <https://linked.opendata.swiss/graph/FOT/didok>" https://lindas-data.ch:8443/lindas/update
-curl -u $STORE_USER:$STORE_PASSWORD -X POST -w "%{http_code}" -s -o /dev/null --data timeout=1000000  --data-urlencode update="LOAD <https://sos-ch-dk-2.exo.io/zazuko-staging/didok.nt.gz> INTO GRAPH <https://linked.opendata.swiss/graph/FOT/didok>" https://lindas-data.ch:8443/lindas/update
-curl -u $STORE_USER:$STORE_PASSWORD -H "Accept: application/sparql-results+json" -X POST -s --data timeout=1000000  --data-urlencode query="SELECT (COUNT(?s) AS ?count) WHERE {  GRAPH <https://linked.opendata.swiss/graph/FOT/didok> {?s ?p ?o}}" https://lindas-data.ch:8443/lindas/query | node node_modules/json/lib/json.js "['results']['bindings'][0]['count']['value']"
+echo "Counted ${COUNT} results."
+
 # Cleanup
-s3cmd -c config/s3cmd del s3://zazuko-staging/didok.nt.gz
+s3cmd -c config/s3cmd del "s3://zazuko-staging/ssz-$CI_COMMIT_REF_SLUG.nt.gz"
